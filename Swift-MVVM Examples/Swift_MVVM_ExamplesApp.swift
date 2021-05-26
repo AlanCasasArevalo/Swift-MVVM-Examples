5e0dc174609dc44d0aeb0895555f0cc6e979cc3b
Alan Casas <alancasasarevalo@gmail.com>
2021-05-26 19:20:33 +0200

    Initial Commit

---
+++ Swift-MVVM Examples/Swift_MVVM_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_MVVM_ExamplesApp.swift
+//  Swift-MVVM Examples
+//
+//  Created by Alan Casas on 26/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_MVVM_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
